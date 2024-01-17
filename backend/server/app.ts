dce3d87e7e50c326a832fd1e065289f8c5964bee
akinyiliz <lizzieke84@gmail.com>
2024-01-17 16:08:50 +0300

    feat: Add CORS configuration

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -1,16 +1,33 @@
+import dotenv from "dotenv";
 import express from "express";
+import cors, { CorsOptions } from "cors";
 
 import connectDB from "../config/db";
 import userRoutes from "../routes/user.routes";
 
+dotenv.config();
+
 const app = express();
+
+// Cross-Origin Resource Sharing
+const allowedOrigins: string[] = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+];
+
+const options: CorsOptions = {
+  origin: allowedOrigins,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(options));
 
 // db connection
 connectDB();
 
 // body parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/auth/users", userRoutes);
