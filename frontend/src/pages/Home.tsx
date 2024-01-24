607ff4142d894cf4154abef2868e4adbbd83f78a
akinyiliz <lizzieke84@gmail.com>
2024-01-24 16:41:39 +0300

    feat: Implememnt fetching products and Add Suspense with a fallback

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,31 @@
-import Categories from "../components/Categories";
+import { Suspense, useEffect, useState } from "react";
+
+import { Product } from "../types/product";
+import Products from "../components/Products";
+import LoadingUI from "../components/LoadingUI";
 
 const Home = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  async function fetchProducts() {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/products");
+
+      const data = await response.json();
+
+      // console.log(data.products);
+      setProducts(data.products);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  });
   return (
-    <>
-      <Categories />
-    </>
+    <Suspense fallback={<LoadingUI />}>
+      <Products products={products || []} />
+    </Suspense>
   );
 };
 
