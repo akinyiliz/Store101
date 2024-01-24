a29ef7f90929929606da58e1e446cc24605cf8f9
akinyiliz <lizzieke84@gmail.com>
2024-01-24 16:43:55 +0300

    feat: Wrap product card with Link to product page when clicked

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -5,24 +5,24 @@ import { Product } from "../types/product";
 
 const ProductCard: FC<Product> = ({ id, title, price, image }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md w-full">
-      <div className="relative w-full h-44 md:h-64 flex items-center justify-center overflow-hidden rounded-lg">
-        <img
-          src={image}
-          alt={title}
-          className="w-[70%] h-full hover:scale-110 transition-transform duration-500 ease-in-out"
-        />
-      </div>
+    <Link to={"/products/" + id}>
+      <div className="bg-white rounded-lg shadow-md w-full">
+        <div className="relative w-full h-44 md:h-64 flex items-center justify-center overflow-hidden rounded-lg">
+          <img
+            src={image}
+            alt={title}
+            className="w-[70%] h-full hover:scale-110 transition-transform duration-500 ease-in-out"
+          />
+        </div>
 
-      <div className="p-4">
-        <Link to={"/products/" + id}>
+        <div className="p-4">
           <h2 className="text-base font-medium text-gray-900">
             {title.split(" ").slice(0, 4).join(" ")}
           </h2>
-        </Link>
-        <p className="mt-1 text-sm text-gray-600">{price}</p>
+          <p className="mt-1 text-sm text-gray-600">{price}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
