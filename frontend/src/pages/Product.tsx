2c7a806f91e9fed2996302b081b3307cc351dd47
akinyiliz <lizzieke84@gmail.com>
2024-01-24 16:38:44 +0300

    feat: Create Product page

---
+++ frontend/src/pages/Product.tsx
@@ -0,0 +1,83 @@
+import { FaStar } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+
+import { Product } from "../types/product";
+import LoadingUI from "../components/LoadingUI";
+
+const ProductPage = () => {
+  const query = useParams();
+  const productId = query.productId;
+
+  const [product, setProduct] = useState<Product>({
+    id: Number(productId),
+    title: "",
+    description: "",
+    category: "",
+    rating: { rate: 0, count: 0 },
+    price: 0,
+    image: "",
+  });
+
+  async function fetchProductDetails() {
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/products/product/${productId}`
+      );
+
+      const data = await response.json();
+
+      // console.log(data);
+      setProduct(data.product);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProductDetails();
+  });
+
+  return (
+    <Suspense fallback={<LoadingUI />}>
+      <section className="py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
+        <div className="px-2 md:w-3/4 md:mx-auto flex flex-col md:flex-row items-center">
+          {/* IMAGE & ADD TO CART BUTTON */}
+          <div className="w-full md:w-1/2 h-full">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-[80%] h-[250px] md:h-[350px] lg:h-[500px] mx-auto md:mx-0 hover:scale-105 transition-transform duration-500 ease-in-out"
+            />
+          </div>
+
+          {/* PRODUCT TITLE, DESCRIPTION & PRICE */}
+          <div className="relative w-full md:w-1/2 flex flex-col gap-2 md:gap-4">
+            <h2 className="font-semibold text-2xl">{product.title}</h2>
+
+            <div className="space-y-2">
+              <p className="text-sm lg:text-lg">{product.description}</p>
+
+              <div className="flex items-center gap-4 lg:gap-8 text-[#808080] text-sm">
+                <p className="capitalize">{product.category}</p>
+                <p className="flex items-center gap-1">
+                  <FaStar className="text-[#FFAE5D]" />
+                  <span>{product.rating?.rate}</span>
+                </p>
+                <p>{product.rating?.count} Ratings</p>
+              </div>
+            </div>
+
+            <p className="text-[#808080] text-xl">${product.price}</p>
+
+            <button className=" py-2 bg-[#FFAE5D] text-lg font-medium">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </section>
+    </Suspense>
+  );
+};
+
+export default ProductPage;
