1290aeecec831d6dd022d1ac1c32360738b5ac41
akinyiliz <lizzieke84@gmail.com>
2024-01-24 16:42:23 +0300

    feat: Add Link on logo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
 
-import { User } from "../types/user";
-import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
+import { User } from "../types/user";
 
 interface NavbarProps {
   user: User | null;
@@ -17,10 +17,12 @@ const Navbar: FC<NavbarProps> = ({ user }) => {
       <nav className="flex flex-col gap-2 px-2 py-3 md:border-b md:border-[#808080]">
         <div className="flex items-center justify-between md:gap-8">
           {/* LOGO */}
-          <h1 className="text-3xl">
-            <span className="font-bold">store</span>
-            <span className="font-normal text-[#FFAE5D]">101</span>
-          </h1>
+          <Link to={"/"}>
+            <h1 className="text-3xl">
+              <span className="font-bold">store</span>
+              <span className="font-normal text-[#FFAE5D]">101</span>
+            </h1>
+          </Link>
 
           {/* CART & LOGIN BUTTON */}
           <ul className="flex items-center gap-2 md:gap-4">
