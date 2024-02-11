0614fd96ee01c4a65a659bda843527d8053e513c
akinyiliz <lizzieke84@gmail.com>
2024-02-11 15:13:29 +0300

    refactor: Fetch products from data folder

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,27 +1,11 @@
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 
-import { Product } from "../types/product";
+import { products } from "../data/products";
+
 import Products from "../components/Products";
 import LoadingUI from "../components/LoadingUI";
 
 const Home = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  async function fetchProducts() {
-    try {
-      const response = await fetch("http://127.0.0.1:8000/api/products");
-
-      const data = await response.json();
-
-      // console.log(data.products);
-      setProducts(data.products);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  });
   return (
     <Suspense fallback={<LoadingUI />}>
       <Products products={products || []} />
