d5726de78c4ce385cb2e99d1afc5bc721bc91247
akinyiliz <lizzieke84@gmail.com>
2024-02-11 15:17:03 +0300

    feat: Implement active category and Add styles for active category

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -1,5 +1,6 @@
 import { MdSearch } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const categories = [
   "electronics",
@@ -9,20 +10,40 @@ const categories = [
 ];
 
 const Searchbar = () => {
+  const [activeCategory, setActiveCategory] = useState<number | null>(null);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/" || location.pathname === "/cart") {
+      setActiveCategory(null);
+    }
+  }, [location.pathname]);
+
   return (
-    <div className="p-2 flex flex-col-reverse lg:flex-row items-center gap-2 md:justify-between">
+    <div className="w-full p-2 flex flex-col-reverse lg:flex-row items-center gap-4 lg:gap-16 md:justify-between">
       {/* CATEGORIES LIST */}
-      <div className="flex items-center flex-wrap gap-1 xl:gap-12">
+      <ul className="w-full md:w-3/4 lg:w-1/2 flex items-center justify-between flex-wrap font-medium">
         {categories.map((category, index) => {
+          const isActive = activeCategory === index;
+
           return (
-            <Link to={"/categories/" + category} key={index}>
-              <div className="text-[#606060] font-medium capitalize text-sm md:text-base">
+            <li
+              key={index}
+              className={`font-medium capitalize text-xs md:text-base ${
+                isActive ? "border-b-4 border-[#FFAE5D]" : ""
+              }`}
+            >
+              <Link
+                to={"/" + category}
+                onClick={() => setActiveCategory(index)}
+              >
                 {category}
-              </div>
-            </Link>
+              </Link>
+            </li>
           );
         })}
-      </div>
+      </ul>
 
       {/* SEARCH BAR */}
       <div className="flex w-full lg:w-1/2 items-center gap-2 border border-[#808080] p-1 rounded-xl">
