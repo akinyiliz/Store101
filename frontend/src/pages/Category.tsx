2995797d1cde8b3bd764d9c75456c2a067bbec72
akinyiliz <lizzieke84@gmail.com>
2024-02-12 18:20:36 +0300

    style: Adding padding top according to screen size

--- frontend/src/pages/Category.tsx
+++ frontend/src/pages/Category.tsx
@@ -10,32 +10,9 @@ interface CategoriesProps {
 }
 
 const Category: FC<CategoriesProps> = ({ category }) => {
-  // const query = useParams();
-  // const category = query.category;
-
-  // const [products, setProducts] = useState<Product[]>([]);
-
-  // async function fetchProducts() {
-  //   try {
-  //     const response = await fetch(
-  //       `https://fakestoreapi.com/products/category/${category}`
-  //     );
-
-  //     const data = await response.json();
-
-  //     // console.log(data);
-  //     setProducts(data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchProducts();
-  // });
   return (
     <Suspense fallback={<LoadingUI />}>
-      <section className="py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
+      <section className="pt-40 lg:pt-36 py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-8">
           {products.map((product) => {
             if (category === product.category) {
