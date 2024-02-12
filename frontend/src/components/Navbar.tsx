c4b9f581acd479636ab30ff5fb02b68f24fc9179
akinyiliz <lizzieke84@gmail.com>
2024-02-12 18:23:50 +0300

    chore: Change to colors configured in tailwind config file

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,14 +16,14 @@ const Navbar: FC<NavbarProps> = ({ user }) => {
   const { getTotalCartItems } = useCart();
 
   return (
-    <section className="xl:max-w-7xl xl:mx-auto w-full h-40 lg:h-28 mb-4 bg-white">
-      <nav className="flex flex-col gap-2 px-2 py-3 md:border-b md:border-[#808080]">
+    <section className="xl:max-w-7xl xl:mx-auto w-full h-36 md:h-40 lg:h-32 mb-4 bg-white fixed z-10 top-0 left-0 right-0">
+      <nav className="flex flex-col gap-2 px-2 py-3 md:px-6 border-b-[3px] border-gray">
         <div className="flex items-center justify-between md:gap-8">
           {/* LOGO */}
           <Link to={"/"}>
             <h1 className="text-3xl">
-              <span className="font-bold">store</span>
-              <span className="font-normal text-[#FFAE5D]">101</span>
+              <span className="font-medium">store</span>
+              <span className="font-semibold text-primaryColor">101</span>
             </h1>
           </Link>
 
@@ -31,17 +31,19 @@ const Navbar: FC<NavbarProps> = ({ user }) => {
           <ul className="flex items-center gap-2 md:gap-4">
             <li className="relative">
               <Link to={"/cart"}>
-                <MdOutlineShoppingCart size={25} />
+                <MdOutlineShoppingCart size={30} />
+                <div className="w-[20px] h-[20px] flex justify-center items-center absolute top-[-5px] left-4 text-sm rounded-full bg-primaryColor font-semibold border border-white">
+                  <small className="text-sm text-center text-white">
+                    {getTotalCartItems()}
+                  </small>
+                </div>
               </Link>
-              <div className="w-[20px] h-[20px] flex justify-center items-center absolute top-[-5px] left-3 text-sm rounded-full bg-[#FFAE5D] text-neutral-800">
-                {getTotalCartItems()}
-              </div>
             </li>
 
             {!user && (
               <li>
                 <Link to={"/login"}>
-                  <button className="font-medium p-2 rounded-lg hover:bg-[#FFAE5D]">
+                  <button className="font-medium p-2 rounded-lg hover:bg-primaryColor hover:text-white">
                     Sign In
                   </button>
                 </Link>
