45da503fd410c4ae6e7b2045092c8c4996dbc956
akinyiliz <lizzieke84@gmail.com>
2024-02-20 17:22:35 +0300

    feat: Add Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { User } from "./types/user";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
@@ -11,71 +8,38 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Category from "./pages/Category";
 import ProductPage from "./pages/Product";
-import CartContextProvider from "./context/CartContext";
+import Footer from "./components/Footer";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
-
-  async function fetchProfile() {
-    const token = localStorage.getItem("access");
-
-    try {
-      const response = await fetch("http://127.0.0.1:8000/auth/users/profile", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const data = await response.json();
-
-      // console.log(data);
-      setUser(data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchProfile();
-  }, []);
-
-  function handleLogout() {
-    localStorage.clear();
-    setUser(null);
-  }
   return (
-    <BrowserRouter>
-      <CartContextProvider>
-        <main className="w-full h-screen bg-white text-black text-base">
-          <Navbar user={user} handleLogout={handleLogout} />
+    <main className="w-full h-screen bg-white text-black text-base flex flex-col">
+      <Navbar />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
 
-            <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart />} />
 
-            <Route
-              path="/electronics"
-              element={<Category category="electronics" />}
-            />
-            <Route
-              path="/jewelery"
-              element={<Category category="jewelery" />}
-            />
-            <Route
-              path="/men's clothing"
-              element={<Category category="men's clothing" />}
-            />
-            <Route
-              path="/women's clothing"
-              element={<Category category="women's clothing" />}
-            />
-            <Route path="/products/:productId" element={<ProductPage />} />
-          </Routes>
-        </main>
-      </CartContextProvider>
-    </BrowserRouter>
+        <Route
+          path="/electronics"
+          element={<Category category="electronics" />}
+        />
+        <Route path="/jewelery" element={<Category category="jewelery" />} />
+        <Route
+          path="/men's clothing"
+          element={<Category category="men's clothing" />}
+        />
+        <Route
+          path="/women's clothing"
+          element={<Category category="women's clothing" />}
+        />
+        <Route path="/products/:productId" element={<ProductPage />} />
+      </Routes>
+
+      <Footer />
+    </main>
   );
 }
 
