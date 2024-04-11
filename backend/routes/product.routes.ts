0173b4f6966b66a1308f0d4a0ed76a9e4c2280b0
akinyiliz <lizzieke84@gmail.com>
2024-04-11 14:39:30 +0300

    feat: Add product routes

--- backend/routes/product.routes.ts
+++ backend/routes/product.routes.ts
@@ -3,12 +3,28 @@ import express from "express";
 import {
   getProducts,
   getSingleProduct,
+  getAllCategories,
+  getProductsInSpecificCategory,
+  uploadImage,
+  addProduct,
+  deleteProduct,
 } from "../controllers/product.controllers";
+import upload from "../services/upload";
 
 const router = express.Router();
 
-router.get("/", getProducts);
+router.get("/all", getProducts);
 
-router.get("/product/:id", getSingleProduct);
+router.post("/image/upload", upload.single("image"), uploadImage);
+
+router.post("/add", addProduct);
+
+router.delete("/delete/:id", deleteProduct);
+
+// router.get("/:id", getSingleProduct);
+
+// router.get("/categories", getAllCategories);
+
+// router.get("/category/:category", getProductsInSpecificCategory);
 
 export default router;
