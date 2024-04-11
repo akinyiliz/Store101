4e54d3e34f6c82d9ef23be86022349b88aee4a2e
akinyiliz <lizzieke84@gmail.com>
2024-04-11 15:19:49 +0300

    feat: Use category section component for various product categories

--- frontend/src/components/Products.tsx
+++ frontend/src/components/Products.tsx
@@ -1,28 +1,50 @@
 import { FC } from "react";
 
-import ProductCard from "./ProductCard";
 import { Product } from "../types/product";
+import CategorySection from "./CategorySection";
 
 interface ProductsProps {
   products: Product[];
 }
 
 const Products: FC<ProductsProps> = ({ products }) => {
+  const menClothes = products.filter(
+    (product) => product.category === "men's clothing"
+  );
+
+  const womenClothes = products.filter(
+    (product) => product.category === "women's clothing"
+  );
+
+  const electronics = products.filter(
+    (product) => product.category === "electronics"
+  );
+
+  const jewelery = products.filter(
+    (product) => product.category === "jewelery"
+  );
   return (
-    <section className="py-6 px-2 pt-40 lg:pt-36 w-full xl:max-w-7xl xl:mx-auto">
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 xl:gap-8">
-        {products.map((product) => {
-          return (
-            <ProductCard
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              price={product.price}
-              image={product.image}
-            />
-          );
-        })}
-      </div>
+    <section className="py-6 px-2 w-full xl:max-w-7xl xl:mx-auto flex flex-col gap-4">
+      <CategorySection
+        name="Men"
+        category="men's clothing"
+        products={menClothes}
+      />
+      <CategorySection
+        name="Women"
+        category="women's clothing"
+        products={womenClothes}
+      />
+      <CategorySection
+        name="Electronics"
+        category="electronics"
+        products={electronics}
+      />
+      <CategorySection
+        name="Jewelery"
+        category="jewelery"
+        products={jewelery}
+      />
     </section>
   );
 };
