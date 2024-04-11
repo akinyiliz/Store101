de013d6efc6e3e82d564c76886308db32589ed0f
akinyiliz <lizzieke84@gmail.com>
2024-04-11 14:38:09 +0300

    feat: Add product mainroute

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import userRoutes from "./user.routes";
+import productRoutes from "./product.routes";
 
 const router = express.Router();
 
 // User Routes
 router.use("/user", userRoutes);
+
+// Product Routes
+router.use("/product", productRoutes);
 
 export default router;
