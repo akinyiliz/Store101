2f1b0e9daa5fee9d0fad9b067d3f1b2e2b4dee0e
akinyiliz <lizzieke84@gmail.com>
2024-04-11 15:21:53 +0300

    feat: Add a banner to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,11 +4,19 @@ import { products } from "../data/products";
 
 import Products from "../components/Products";
 import LoadingUI from "../components/LoadingUI";
+import Banner from "../assets/banner.jpg";
 
 const Home = () => {
   return (
     <Suspense fallback={<LoadingUI />}>
-      <Products products={products || []} />
+      <div className="pt-40 xl:max-w-7xl xl:mx-auto">
+        <img
+          src={Banner}
+          alt="Banner"
+          className=" h-60 lg:h-96 w-full object-cover"
+        />
+        <Products products={products || []} />
+      </div>
     </Suspense>
   );
 };
