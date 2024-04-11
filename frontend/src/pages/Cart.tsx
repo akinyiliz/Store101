ff1bbecf442e5024d107837d0d64df9952c3246b
akinyiliz <lizzieke84@gmail.com>
2024-04-11 15:21:09 +0300

    refactor: Add cart title and comment out checkout button

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -15,18 +15,13 @@ const Cart: FC = () => {
     <section className="p-4 pt-40 lg:pt-36 w-full xl:max-w-7xl xl:mx-auto">
       {getTotalCartItems() === 0 ? (
         <div className="flex items-center justify-center mt-20">
-          <h2 className="text-gray text-xl lg:text-3xl">Cart is empty.</h2>
+          <h2 className="text-gray text-2xl lg:text-3xl">Cart is empty.</h2>
         </div>
       ) : (
         <div className="md:px-4 lg:px-16">
-          <div className="cart-container-format text-lg font-semibold">
-            <p>Products</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
-            <p>Remove</p>
-          </div>
+          <h3 className="pb-2 text-2xl font-bold text-primaryColor">
+            Cart ({getTotalCartItems()})
+          </h3>
           <hr className="h-[3px] bg-gray border-0" />
           {products.map((product) => {
             if (cart[product.id] > 0) {
@@ -60,7 +55,7 @@ const Cart: FC = () => {
             return null;
           })}
           <div className="flex flex-col-reverse md:flex-row my-10 gap-5">
-            <div className="flex-1 flex flex-col md:mr-20 gap-3 md:gap-5">
+            <div className="w-full md:w-1/2 flex flex-col md:mr-20 gap-3 md:gap-5">
               <h1 className="text-2xl font-semibold">Cart Total</h1>
 
               <div className="text-sm md:text-base">
@@ -78,18 +73,18 @@ const Cart: FC = () => {
 
                 <hr />
 
-                <div className="flex justify-between py-3 font-semibold">
+                <div className="text-lg flex justify-between py-3 font-semibold">
                   <h3>Total</h3>
                   <h3>${getTotalCartAmount().toFixed(2)}</h3>
                 </div>
               </div>
 
-              <button className="py-2 md:py-4 outline-none border-none bg-primaryColor text-white text-base font-semibold cursor-pointer">
+              {/* <button disabled className="py-2 md:py-4 outline-none border-none bg-primaryColor text-white text-base font-semibold cursor-pointer">
                 Proceed to Checkout
-              </button>
+              </button> */}
             </div>
 
-            <div className="flex-1 text-base font-medium">
+            {/* <div className="flex-1 text-base font-medium">
               <p className="text-secondaryColor">
                 If you have a promo code, Enter it here
               </p>
@@ -104,7 +99,7 @@ const Cart: FC = () => {
                   Submit
                 </button>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       )}
