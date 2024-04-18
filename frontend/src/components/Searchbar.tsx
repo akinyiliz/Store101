426f467442d131e5acdbc1eb813028ed91de3862
akinyiliz <lizzieke84@gmail.com>
2024-04-18 15:46:59 +0300

    refactor: Replace Link with NavLink

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -1,6 +1,5 @@
 import { MdSearch } from "react-icons/md";
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const categories = [
   "electronics",
@@ -10,41 +9,25 @@ const categories = [
 ];
 
 const Searchbar = () => {
-  const [activeCategory, setActiveCategory] = useState<number | null>(null);
-
-  const location = useLocation();
-
-  useEffect(() => {
-    if (
-      location.pathname === "/" ||
-      location.pathname === "/cart" ||
-      location.pathname === "/register" ||
-      location.pathname === "/login"
-    ) {
-      setActiveCategory(null);
-    }
-  }, [location.pathname]);
-
   return (
-    <div className="md:px-6 w-full p-2 flex flex-col-reverse lg:flex-row items-center gap-2 md:gap-4 lg:gap-16 md:justify-between">
+    <div className="md:px-6 w-full py-2 px-4 flex flex-col-reverse lg:flex-row items-center gap-2 md:gap-4 lg:gap-16 md:justify-between">
       {/* CATEGORIES LIST */}
       <ul className="w-full md:w-3/4 lg:w-1/2 flex items-center justify-between flex-wrap font-medium">
         {categories.map((category, index) => {
-          const isActive = activeCategory === index;
-
           return (
-            <li
-              key={index}
-              className={`font-medium capitalize text-xs md:text-base ${
-                isActive ? "border-b-4 border-primaryColor" : ""
-              }`}
-            >
-              <Link
+            <li key={index} className="">
+              <NavLink
                 to={"/" + category}
-                onClick={() => setActiveCategory(index)}
+                className={({ isActive }) =>
+                  `font-medium capitalize text-xs md:text-base ${
+                    isActive
+                      ? "border-b-4 border-primaryColor duration-100"
+                      : ""
+                  }`
+                }
               >
                 {category}
-              </Link>
+              </NavLink>
             </li>
           );
         })}
@@ -53,10 +36,11 @@ const Searchbar = () => {
       {/* SEARCH BAR */}
       <div className="flex w-full lg:w-1/2 items-center gap-2 border border-[#808080] p-1 rounded-xl">
         <MdSearch size={22} className="text-[#808080]" />
+
         <input
           type="text"
           placeholder="Search for products"
-          className="flex-1 focus:outline-none"
+          className="w-full focus:outline-none"
         />
       </div>
     </div>
