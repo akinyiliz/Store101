c965f1dcbbe2416961695d1c1524b6ea12e82174
akinyiliz <lizzieke84@gmail.com>
2024-05-09 14:32:55 +0300

    feat: Add search functionality

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -1,5 +1,6 @@
 import { MdSearch } from "react-icons/md";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useSearch } from "../context/SearchContext";
 
 const categories = [
   "electronics",
@@ -9,6 +10,15 @@ const categories = [
 ];
 
 const Searchbar = () => {
+  const { searchTerm, setSearchTerm } = useSearch();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
   return (
     <div className="md:px-6 w-full py-2 px-4 flex flex-col-reverse lg:flex-row items-center gap-2 md:gap-4 lg:gap-16 md:justify-between">
       {/* CATEGORIES LIST */}
@@ -37,11 +47,14 @@ const Searchbar = () => {
       <div className="flex w-full lg:w-1/2 items-center gap-2 border border-[#808080] p-1 rounded-xl">
         <MdSearch size={22} className="text-[#808080]" />
 
-        <input
-          type="text"
-          placeholder="Search for products"
-          className="w-full focus:outline-none"
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search for products"
+            className="w-full focus:outline-none"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
       </div>
     </div>
   );
