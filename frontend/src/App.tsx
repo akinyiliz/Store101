927214046eb611c666c4e36687f0355f5cba47c6
akinyiliz <lizzieke84@gmail.com>
2024-05-09 14:31:49 +0300

    feat: Add SearchResults page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,13 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import ProductPage from "./pages/Product";
 import Footer from "./components/Footer";
+import { useSearch } from "./context/SearchContext";
+
+import SearchResultsPage from "./pages/SearchResultsPage";
 
 function App() {
+  const { searchTerm } = useSearch();
+
   return (
     <main className="w-full h-screen bg-white text-black text-base flex flex-col">
       <Navbar />
@@ -33,6 +38,12 @@ function App() {
           element={<Category category="women's clothing" />}
         />
         <Route path="/products/:productId" element={<ProductPage />} />
+        <Route
+          path={`/search/:${searchTerm}`}
+          element={<SearchResultsPage />}
+        />
+
+        <Route path="*" element={<Home />} />
       </Routes>
 
       <Footer />
