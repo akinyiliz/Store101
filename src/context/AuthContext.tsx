cba461f083ec82c556393597a2b866dc37b7cf2d
akinyiliz <lizzieke84@gmail.com>
2024-08-20 18:43:18 +0300

    feat: Add loading and userDEtails states to auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-refresh/only-export-components */
 
-import { createContext, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext, useContext, useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { User } from "../types/user";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -11,23 +11,69 @@ interface AuthContextType {
   register: (name: string, username: string, password: string) => Promise<void>;
   login: (username: string, password: string) => Promise<void>;
   logout: () => void;
+  loading: boolean;
+  userDetails: User | null;
 }
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(
-    localStorage.getItem("access")
+    localStorage.getItem("user-access")
   );
+  const [loading, setLoading] = useState(false);
+  const [userDetails, setUserDetails] = useState<User | null>(null);
+
   const isAuthenticated = !!token;
 
-  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated && token) {
+      fetchUserDetails(token);
+    }
+  }, [isAuthenticated, token]);
+
+  const fetchUserDetails = async (token: string) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/user/profile`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+
+      if (data.success) {
+        setUserDetails(data.data);
+      }
+    } catch (error: any) {
+      const errorMessage =
+        error.message || "Failed to fetch user details. Try again later.";
+      console.error(errorMessage, error);
+      toast.error(errorMessage, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const register = async (
     username: string,
     email: string,
     password: string
   ) => {
+    setLoading(true);
     try {
       const response = await fetch(
         `${import.meta.env.VITE_BASE_URL}/user/register`,
@@ -43,10 +89,7 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
       const data = await response.json();
 
       if (data.success) {
-        setToken(data.token);
-        localStorage.setItem("access", data.token);
-
-        toast.success(data.message, {
+        toast.success(`${data.message}. Please login!`, {
           position: "top-center",
           autoClose: 5000,
           hideProgressBar: false,
@@ -56,7 +99,7 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
           progress: undefined,
         });
       } else {
-        toast.error(data.error, {
+        toast.error(data.message, {
           position: "top-center",
           autoClose: 5000,
           hideProgressBar: false,
@@ -66,8 +109,10 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
           progress: undefined,
         });
       }
-    } catch (error) {
-      toast.error("Something went wrong! Try again later.", {
+    } catch (error: any) {
+      const errorMessage =
+        error.message || "Something went wrong! Try again later.";
+      toast.error(errorMessage, {
         position: "top-center",
         autoClose: 5000,
         hideProgressBar: false,
@@ -76,10 +121,13 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
         draggable: true,
         progress: undefined,
       });
+    } finally {
+      setLoading(false);
     }
   };
 
   const login = async (email: string, password: string) => {
+    setLoading(true);
     try {
       const response = await fetch(
         `${import.meta.env.VITE_BASE_URL}/user/login`,
@@ -96,7 +144,7 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
 
       if (data.success) {
         setToken(data.token);
-        localStorage.setItem("access", data.token);
+        localStorage.setItem("user-access", data.token);
 
         toast.success(data.message, {
           position: "top-center",
@@ -107,10 +155,8 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
           draggable: true,
           progress: undefined,
         });
-        navigate("/");
       } else {
-        navigate("/login");
-        toast.error(data.error, {
+        toast.error(data.message, {
           position: "top-center",
           autoClose: 5000,
           hideProgressBar: false,
@@ -120,8 +166,10 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
           progress: undefined,
         });
       }
-    } catch (error) {
-      toast.error("Something went wrong! Try again later.", {
+    } catch (error: any) {
+      const errorMessage =
+        error.message || "Something went wrong! Try again later.";
+      toast.error(errorMessage, {
         position: "top-center",
         autoClose: 5000,
         hideProgressBar: false,
@@ -130,6 +178,8 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
         draggable: true,
         progress: undefined,
       });
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -144,6 +194,8 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
     register,
     login,
     isAuthenticated,
+    loading,
+    userDetails,
   };
 
   return (
