b84677f9e02ae8b67bc0a985f2ff0e41446f7c6a
akinyiliz <lizzieke84@gmail.com>
2024-08-20 18:45:16 +0300

    chore: Change id to _id

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -2,19 +2,17 @@ import { FaStar } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Product } from "../types/product";
-import LoadingUI from "../components/LoadingUI";
-import { products } from "../data/products";
 import { useCart } from "../context/CartContext";
 
 const ProductPage = () => {
   const query = useParams();
-  const productId = Number(query.productId);
+  const productId = query.productId ?? "";
 
   const [product, setProduct] = useState<Product>({
-    id: productId,
+    _id: productId,
     title: "",
     description: "",
     category: "",
@@ -24,11 +22,11 @@ const ProductPage = () => {
     image: "",
   });
 
-  const { addToCart } = useCart();
+  const { addToCart, products } = useCart();
 
   // Getting product clicked using its id
   useEffect(() => {
-    const actualProduct = products.find((product) => product.id === productId);
+    const actualProduct = products.find((product) => product._id === productId);
 
     if (actualProduct) {
       setProduct(actualProduct);
@@ -37,7 +35,7 @@ const ProductPage = () => {
 
   // Handling AddToCart functionality using addToCart function from CartContext
   const handleAddToCart = () => {
-    addToCart(product.id);
+    addToCart(product._id);
     toast.success("Product added successfully", {
       position: "top-center",
       autoClose: 5000,
@@ -50,48 +48,46 @@ const ProductPage = () => {
   };
 
   return (
-    <Suspense fallback={<LoadingUI />}>
-      <section className="pt-40 lg:pt-36 py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
-        <div className="px-2 md:w-3/4 md:mx-auto flex flex-col md:flex-row items-center">
-          {/* IMAGE & ADD TO CART BUTTON */}
-          <div className="w-full md:w-1/2 h-full">
-            <img
-              src={product.image}
-              alt={product.title}
-              className="w-[80%] h-[300px] md:h-[350px] lg:h-[500px] mx-auto md:mx-0 hover:scale-105 transition-transform duration-500 ease-in-out"
-            />
-          </div>
-
-          {/* PRODUCT TITLE, DESCRIPTION & PRICE */}
-          <div className="relative w-full md:w-1/2 flex flex-col gap-2 md:gap-4">
-            <h2 className="font-semibold text-2xl">{product.title}</h2>
-
-            <div className="space-y-2">
-              <p className="text-sm lg:text-lg">{product.description}</p>
-
-              <div className="flex items-center gap-4 lg:gap-8 font-light text-sm">
-                <p className="capitalize">{product.category}</p>
-                <p className="flex items-center justify-center gap-1">
-                  <FaStar className="text-primaryColor" />
-                  <span>{product.rate}</span>
-                </p>
-                <p>{product.reviews} Reviews</p>
-              </div>
-            </div>
-
-            <p className="font-light text-xl">${product.price}</p>
-
-            <button
-              onClick={handleAddToCart}
-              className="uppercase py-2 bg-primaryColor text-white text-lg font-medium"
-            >
-              Add to cart
-            </button>
-          </div>
+    <section className="pt-40 lg:pt-36 py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
+      <div className="px-2 md:w-3/4 md:mx-auto flex flex-col md:flex-row items-center">
+        {/* IMAGE & ADD TO CART BUTTON */}
+        <div className="w-full md:w-1/2 h-full">
+          <img
+            src={product.image}
+            alt={product.title}
+            className="w-[80%] h-[300px] md:h-[350px] lg:h-[500px] mx-auto md:mx-0 hover:scale-105 transition-transform duration-500 ease-in-out"
+          />
         </div>
-      </section>
+
+        {/* PRODUCT TITLE, DESCRIPTION & PRICE */}
+        <div className="relative w-full md:w-1/2 flex flex-col gap-2 md:gap-4">
+          <h2 className="font-semibold text-2xl">{product.title}</h2>
+
+          <div className="space-y-2">
+            <p className="text-sm lg:text-lg">{product.description}</p>
+
+            <div className="flex items-center gap-4 lg:gap-8 font-light text-sm">
+              <p className="capitalize">{product.category}</p>
+              <p className="flex items-center justify-center gap-1">
+                <FaStar className="text-primaryColor" />
+                <span>{product.rate}</span>
+              </p>
+              <p>{product.reviews} Reviews</p>
+            </div>
+          </div>
+
+          <p className="font-light text-xl">Ksh. {product.price}</p>
+
+          <button
+            onClick={handleAddToCart}
+            className="uppercase py-2 bg-primaryColor text-white text-lg font-medium"
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
       <ToastContainer />
-    </Suspense>
+    </section>
   );
 };
 
