b8b529ffccc6df3bee1bbff37bc0e0c9bd206aca
akinyiliz <lizzieke84@gmail.com>
2024-08-20 18:39:43 +0300

    feat: Get products from cart context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,18 @@
-import { Suspense } from "react";
-
-import { products } from "../data/products";
-
 import Products from "../components/Products";
-import LoadingUI from "../components/LoadingUI";
 import Banner from "../assets/banner.jpg";
+import { useCart } from "../context/CartContext";
 
 const Home = () => {
+  const { products } = useCart();
   return (
-    <Suspense fallback={<LoadingUI />}>
-      <div className="pt-40 xl:max-w-7xl xl:mx-auto">
-        <img
-          src={Banner}
-          alt="Banner"
-          className=" h-60 lg:h-96 w-full object-cover"
-        />
-        <Products products={products || []} />
-      </div>
-    </Suspense>
+    <div className="pt-40 xl:max-w-7xl xl:mx-auto">
+      <img
+        src={Banner}
+        alt="Banner"
+        className=" h-60 lg:h-96 w-full object-cover"
+      />
+      <Products products={products || []} />
+    </div>
   );
 };
 
