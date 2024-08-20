fd30dc504a8d0550275ddd2d61ad51f88b055a8b
akinyiliz <lizzieke84@gmail.com>
2024-08-20 18:58:12 +0300

    chore: Change id to _id

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,35 +1,35 @@
-import { FC, Suspense } from "react";
-
-import { products } from "../data/products";
-
-import LoadingUI from "../components/LoadingUI";
+import { FC } from "react";
 import ProductCard from "../components/ProductCard";
+import { useCart } from "../context/CartContext";
 
 interface CategoriesProps {
   category: string;
 }
 
 const Category: FC<CategoriesProps> = ({ category }) => {
+  const { products } = useCart();
+
+  if (products.length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
-    <Suspense fallback={<LoadingUI />}>
-      <section className="pt-40 lg:pt-36 py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-8">
-          {products.map((product) => {
-            if (category === product.category) {
-              return (
-                <ProductCard
-                  key={product.id}
-                  id={product.id}
-                  title={product.title}
-                  price={product.price}
-                  image={product.image}
-                />
-              );
-            }
-          })}
-        </div>
-      </section>
-    </Suspense>
+    <section className="pt-40 lg:pt-36 py-4 px-2 w-full xl:max-w-7xl xl:mx-auto">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-8">
+        {products.map((product) => {
+          if (category === product.category) {
+            return (
+              <ProductCard
+                key={product._id}
+                _id={product._id}
+                title={product.title}
+                price={product.price}
+                image={product.image}
+              />
+            );
+          }
+        })}
+      </div>
+    </section>
   );
 };
 
