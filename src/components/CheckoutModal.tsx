7819e5e5a5a73becba13cc14a056177f226080a5
akinyiliz <lizzieke84@gmail.com>
2024-08-20 18:46:07 +0300

    feat: Create checkout modal

---
+++ src/components/CheckoutModal.tsx
@@ -0,0 +1,13 @@
+function CheckoutModal() {
+  return (
+    <div className="bg-white absolute w-[90%] md:1/2 lg:w-1/4 h-[150px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md">
+      <h1 className="text-3xl text-center py-4 font-bold">Checkout</h1>
+
+      <p className="text-center text-lg">
+        Thank you for shopping with Store101!
+      </p>
+    </div>
+  );
+}
+
+export default CheckoutModal;
